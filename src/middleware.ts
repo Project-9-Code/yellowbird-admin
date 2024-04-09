232bb81399b899d8d044f4cd0dc48cf7bfccf4a4
CB <chris@project9.studio>
2024-04-09 11:08:48 -0400

    chore: updated env and api auth routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -41,20 +41,8 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - api (API routes)
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     */
-    {
-      source: '/((?!api|_next/static|_next/image|favicon.ico).*)',
-      missing: [
-        { type: 'header', key: 'next-router-prefetch' },
-        { type: 'header', key: 'purpose', value: 'prefetch' },
-      ],
-    },
-    "/api/login", "/api/logout"
-  ],
+    '/((?!_next|favicon.ico|__/auth|__/firebase|api|.*\\.).*)',
+    '/api/login',
+    '/api/logout'
+  ]
 }
