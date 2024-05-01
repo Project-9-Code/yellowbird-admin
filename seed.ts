7c0849dd2efd3754f76265c3ff42484b26fdb55f
CB <chris@project9.studio>
2024-05-01 16:36:04 -0400

    Seed db on reset

--- seed.ts
+++ seed.ts
@@ -17,59 +17,46 @@ const main = async () => {
   await seed.$resetDatabase();
 
   // Create intro user
-  /*await seed.user([{
+  await seed.users([{
     id: introUserId,
-    name: "Samantha Taylor",
-    organization: "Yellowbird",
-    email: "intro@yellowbird.com",
-    avatar_url: "https://s3-alpha-sig.figma.com/img/a122/e47f/b96ab35580fed3a624bcb755939a3c62?Expires=1715558400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=EIUIc~dbpR4tIFVwqgN9ulbJ1-7lVD-MsAT0U-8GS7m6ciSZfSiniq77WOqMeehB~Sfawgo9~5czRW3bzCdPplpbenWE8hAQMx0s-i18JKUEbRihdJjJJBA1HOwNonPCSgcf1QSx0xVv460P8sxMICU7073lAkM8hcecYJeC8Zf02ZKeg064PkxJtg8dVjOHeXYi8CJVpYu4uHMAqtlz9iHqeFJU7OCNW7V6SWa62cr1f1cZS80bYH84JQ6~H79srNAcFCG7JX1kboZ7N5FlcAeC2QyybQ31t8TR3cErUdqK5D5vWJoNRObJoHhtRTIDZCxpz3bZSSYkfZasYvwcZA__"
+    email: "samantha@yellowbird.com",
   }]);
 
-  // Create intro course
-  await seed.course([{
+  // Create intro course with intro lesson
+  await seed.courses([{
     id: introCourseId,
     title: "Intro to Yellowbird",
-    description: "You will learn the difference between a credit card and a debit card. They look the same, but have different purposes.",
+    course_description: "You will learn the difference between a credit card and a debit card. They look the same, but have different purposes.",
     created_by: introUserId,
     active_lessons: 0,
     archived_lessons: 0,
     draft_lessons: 0,
-  }]);
-
-  // Create intro lesson
-  await seed.lesson([{
-    id: introLessonId,
-    title: "Intro to Yellowbird",
-    description: "You will learn the difference between a credit card and a debit card. They look the same, but have different purposes.",
-    course_id: introCourseId,
-    author: introUserId,
-    order: 0,
-    tags: ["intro"],
-    blocks: JSON.stringify([
-      {
-        id: "0",
-        type: "TEXT",
-        order: 0,
-        screenContent: "Underwriting is an evaluation done by the insurance company to see if it’s worth the risk of providing insurance coverage. In other words, can the insurance company make money from the policy?"
-      },
-      {
-        id: "1",
-        type: "MEDIA",
-        order: 1,
-        mediaUrl: "https://firebasestorage.googleapis.com/v0/b/yellowbird-4e1b8.appspot.com/o/public%2Flesson%2Fblock2%2Fmedia?alt=media&token=3e3bccd2-80cc-44f6-aadf-f05bbada65d5",
-        screenContent: "Some screens will have new words. User can tap the highlighted word to reveal a card overlay."
-      },
-      {
-        id: "2",
-        type: "CHOICE",
-        order: 2,
+    lessons: [{
+      id: introLessonId,
+      title: "Intro to Yellowbird",
+      lesson_description: "You will learn the difference between a credit card and a debit card. They look the same, but have different purposes.",
+      author: introUserId,
+      lesson_order: 0,
+      tags: ["intro"],
+      lesson_blocks: [{
+        block_order: 0,
+        block_type: "TEXT",
+        screen_content: "Underwriting is an evaluation done by the insurance company to see if it’s worth the risk of providing insurance coverage. In other words, can the insurance company make money from the policy?"
+      }, {
+        block_type: "MEDIA",
+        block_order: 1,
+        media_url: "https://firebasestorage.googleapis.com/v0/b/yellowbird-4e1b8.appspot.com/o/public%2Flesson%2Fblock2%2Fmedia?alt=media&token=3e3bccd2-80cc-44f6-aadf-f05bbada65d5",
+        screen_content: "Some screens will have new words. User can tap the highlighted word to reveal a card overlay."
+      }, {
+        block_type: "CHOICE",
+        block_order: 2,
         question: "A bear market is when the market is showing a steady decline over a period of 30-days?",
         answer_options: ["True", "False"],
         answers: ["True"],
         points: 10
-      }
-    ]),
-  }]);*/
+      }],
+    }]
+  }]);
 
   process.exit();
 };
---
+++ supabase/migrations/20240501161138_initial.sql
@@ -0,0 +1,258 @@
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+CREATE EXTENSION IF NOT EXISTS "pg_net" WITH SCHEMA "extensions";
+
+CREATE EXTENSION IF NOT EXISTS "pgsodium" WITH SCHEMA "pgsodium";
+
+COMMENT ON SCHEMA "public" IS 'standard public schema';
+
+CREATE EXTENSION IF NOT EXISTS "pg_graphql" WITH SCHEMA "graphql";
+
+CREATE EXTENSION IF NOT EXISTS "pg_stat_statements" WITH SCHEMA "extensions";
+
+CREATE EXTENSION IF NOT EXISTS "pgcrypto" WITH SCHEMA "extensions";
+
+CREATE EXTENSION IF NOT EXISTS "pgjwt" WITH SCHEMA "extensions";
+
+CREATE EXTENSION IF NOT EXISTS "supabase_vault" WITH SCHEMA "vault";
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA "extensions";
+
+CREATE TYPE "public"."block_types" AS ENUM (
+    'TEXT',
+    'MEDIA',
+    'VIDEO',
+    'CHOICE',
+    'MULTI_CHOICE',
+    'MULTI_SELECT'
+);
+
+ALTER TYPE "public"."block_types" OWNER TO "postgres";
+
+CREATE OR REPLACE FUNCTION "public"."handle_new_user"() RETURNS "trigger"
+    LANGUAGE "plpgsql" SECURITY DEFINER
+    AS $$
+begin
+  insert into public.profiles (id, full_name, avatar_url)
+  values (new.id, new.raw_user_meta_data->>'full_name', new.raw_user_meta_data->>'avatar_url');
+  return new;
+end;
+$$;
+
+ALTER FUNCTION "public"."handle_new_user"() OWNER TO "postgres";
+
+SET default_tablespace = '';
+
+SET default_table_access_method = "heap";
+
+CREATE TABLE IF NOT EXISTS "public"."bookmarks" (
+    "lesson_id" "uuid" NOT NULL,
+    "user_id" "uuid" NOT NULL,
+    "attempts" integer DEFAULT 0 NOT NULL,
+    "points" integer DEFAULT 0 NOT NULL,
+    "completed" boolean DEFAULT false NOT NULL,
+    "completed_at" timestamp with time zone,
+    "created_at" timestamp with time zone DEFAULT "timezone"('utc'::"text", "now"()) NOT NULL,
+    "updated_at" timestamp with time zone DEFAULT "timezone"('utc'::"text", "now"()) NOT NULL
+);
+
+ALTER TABLE "public"."bookmarks" OWNER TO "postgres";
+
+CREATE TABLE IF NOT EXISTS "public"."courses" (
+    "id" "uuid" DEFAULT "gen_random_uuid"() NOT NULL,
+    "title" "text",
+    "course_description" "text",
+    "cover_photo_url" "text",
+    "active_lessons" integer DEFAULT 0 NOT NULL,
+    "archived_lessons" integer DEFAULT 0 NOT NULL,
+    "draft_lessons" integer DEFAULT 0 NOT NULL,
+    "created_at" timestamp with time zone DEFAULT "timezone"('utc'::"text", "now"()) NOT NULL,
+    "updated_at" timestamp with time zone DEFAULT "timezone"('utc'::"text", "now"()) NOT NULL,
+    "created_by" "uuid" DEFAULT "auth"."uid"(),
+    "updated_by" "uuid" DEFAULT "auth"."uid"()
+);
+
+ALTER TABLE "public"."courses" OWNER TO "postgres";
+
+CREATE TABLE IF NOT EXISTS "public"."lesson_blocks" (
+    "id" "uuid" DEFAULT "gen_random_uuid"() NOT NULL,
+    "lesson" "uuid" NOT NULL,
+    "block_type" "public"."block_types" NOT NULL,
+    "media_url" "text",
+    "screen_content" "text",
+    "question" "text",
+    "answers" "text"[],
+    "answer_options" "text"[],
+    "points" integer,
+    "block_order" integer DEFAULT 0 NOT NULL,
+    "created_at" timestamp with time zone DEFAULT "timezone"('utc'::"text", "now"()) NOT NULL,
+    "updated_at" timestamp with time zone DEFAULT "timezone"('utc'::"text", "now"()) NOT NULL,
+    "created_by" "uuid" DEFAULT "auth"."uid"(),
+    "updated_by" "uuid" DEFAULT "auth"."uid"()
+);
+
+ALTER TABLE "public"."lesson_blocks" OWNER TO "postgres";
+
+CREATE TABLE IF NOT EXISTS "public"."lessons" (
+    "id" "uuid" DEFAULT "gen_random_uuid"() NOT NULL,
+    "course" "uuid",
+    "author" "uuid",
+    "title" "text",
+    "lesson_description" "text",
+    "lesson_order" integer DEFAULT 0 NOT NULL,
+    "tags" "text"[],
+    "created_at" timestamp with time zone DEFAULT "timezone"('utc'::"text", "now"()) NOT NULL,
+    "updated_at" timestamp with time zone DEFAULT "timezone"('utc'::"text", "now"()) NOT NULL,
+    "created_by" "uuid" DEFAULT "auth"."uid"(),
+    "updated_by" "uuid" DEFAULT "auth"."uid"()
+);
+
+ALTER TABLE "public"."lessons" OWNER TO "postgres";
+
+CREATE TABLE IF NOT EXISTS "public"."profiles" (
+    "id" "uuid" NOT NULL,
+    "updated_at" timestamp with time zone,
+    "username" "text",
+    "full_name" "text",
+    "avatar_url" "text",
+    "website" "text",
+    "organization" "text",
+    CONSTRAINT "username_length" CHECK (("char_length"("username") >= 3))
+);
+
+ALTER TABLE "public"."profiles" OWNER TO "postgres";
+
+ALTER TABLE ONLY "public"."bookmarks"
+    ADD CONSTRAINT "bookmarks_pkey" PRIMARY KEY ("lesson_id", "user_id");
+
+ALTER TABLE ONLY "public"."courses"
+    ADD CONSTRAINT "courses_pkey" PRIMARY KEY ("id");
+
+ALTER TABLE ONLY "public"."lesson_blocks"
+    ADD CONSTRAINT "lesson_blocks_pkey" PRIMARY KEY ("id");
+
+ALTER TABLE ONLY "public"."lessons"
+    ADD CONSTRAINT "lessons_pkey" PRIMARY KEY ("id");
+
+ALTER TABLE ONLY "public"."profiles"
+    ADD CONSTRAINT "profiles_pkey" PRIMARY KEY ("id");
+
+ALTER TABLE ONLY "public"."profiles"
+    ADD CONSTRAINT "profiles_username_key" UNIQUE ("username");
+
+ALTER TABLE ONLY "public"."bookmarks"
+    ADD CONSTRAINT "bookmarks_lesson_id_fkey" FOREIGN KEY ("lesson_id") REFERENCES "public"."lessons"("id") ON UPDATE CASCADE ON DELETE CASCADE;
+
+ALTER TABLE ONLY "public"."bookmarks"
+    ADD CONSTRAINT "bookmarks_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "public"."profiles"("id") ON UPDATE CASCADE ON DELETE CASCADE;
+
+ALTER TABLE ONLY "public"."courses"
+    ADD CONSTRAINT "courses_created_by_fkey" FOREIGN KEY ("created_by") REFERENCES "public"."profiles"("id") ON UPDATE CASCADE ON DELETE SET NULL;
+
+ALTER TABLE ONLY "public"."courses"
+    ADD CONSTRAINT "courses_updated_by_fkey" FOREIGN KEY ("updated_by") REFERENCES "public"."profiles"("id") ON UPDATE CASCADE ON DELETE SET NULL;
+
+ALTER TABLE ONLY "public"."lesson_blocks"
+    ADD CONSTRAINT "lesson_blocks_created_by_fkey" FOREIGN KEY ("created_by") REFERENCES "public"."profiles"("id") ON UPDATE CASCADE ON DELETE SET NULL;
+
+ALTER TABLE ONLY "public"."lesson_blocks"
+    ADD CONSTRAINT "lesson_blocks_lesson_fkey" FOREIGN KEY ("lesson") REFERENCES "public"."lessons"("id");
+
+ALTER TABLE ONLY "public"."lesson_blocks"
+    ADD CONSTRAINT "lesson_blocks_updated_by_fkey" FOREIGN KEY ("updated_by") REFERENCES "public"."profiles"("id") ON UPDATE CASCADE ON DELETE SET NULL;
+
+ALTER TABLE ONLY "public"."lessons"
+    ADD CONSTRAINT "lessons_author_fkey" FOREIGN KEY ("author") REFERENCES "public"."profiles"("id") ON UPDATE CASCADE ON DELETE SET NULL;
+
+ALTER TABLE ONLY "public"."lessons"
+    ADD CONSTRAINT "lessons_course_fkey" FOREIGN KEY ("course") REFERENCES "public"."courses"("id") ON UPDATE CASCADE ON DELETE SET NULL;
+
+ALTER TABLE ONLY "public"."lessons"
+    ADD CONSTRAINT "lessons_created_by_fkey" FOREIGN KEY ("created_by") REFERENCES "public"."profiles"("id") ON UPDATE CASCADE ON DELETE SET NULL;
+
+ALTER TABLE ONLY "public"."lessons"
+    ADD CONSTRAINT "lessons_updated_by_fkey" FOREIGN KEY ("updated_by") REFERENCES "public"."profiles"("id") ON UPDATE CASCADE ON DELETE SET NULL;
+
+ALTER TABLE ONLY "public"."profiles"
+    ADD CONSTRAINT "profiles_id_fkey" FOREIGN KEY ("id") REFERENCES "auth"."users"("id") ON DELETE CASCADE;
+
+CREATE POLICY "Public profiles are viewable by everyone." ON "public"."profiles" FOR SELECT USING (true);
+
+CREATE POLICY "Users can insert their own profile." ON "public"."profiles" FOR INSERT WITH CHECK ((( SELECT "auth"."uid"() AS "uid") = "id"));
+
+CREATE POLICY "Users can update own profile." ON "public"."profiles" FOR UPDATE USING ((( SELECT "auth"."uid"() AS "uid") = "id"));
+
+ALTER TABLE "public"."profiles" ENABLE ROW LEVEL SECURITY;
+ALTER TABLE "public"."courses" ENABLE ROW LEVEL SECURITY;
+ALTER TABLE "public"."lessons" ENABLE ROW LEVEL SECURITY;
+ALTER TABLE "public"."lesson_blocks" ENABLE ROW LEVEL SECURITY;
+ALTER TABLE "public"."bookmarks" ENABLE ROW LEVEL SECURITY;
+
+ALTER PUBLICATION "supabase_realtime" OWNER TO "postgres";
+
+GRANT USAGE ON SCHEMA "public" TO "postgres";
+GRANT USAGE ON SCHEMA "public" TO "anon";
+GRANT USAGE ON SCHEMA "public" TO "authenticated";
+GRANT USAGE ON SCHEMA "public" TO "service_role";
+
+GRANT ALL ON FUNCTION "public"."handle_new_user"() TO "anon";
+GRANT ALL ON FUNCTION "public"."handle_new_user"() TO "authenticated";
+GRANT ALL ON FUNCTION "public"."handle_new_user"() TO "service_role";
+
+GRANT ALL ON TABLE "public"."bookmarks" TO "anon";
+GRANT ALL ON TABLE "public"."bookmarks" TO "authenticated";
+GRANT ALL ON TABLE "public"."bookmarks" TO "service_role";
+
+GRANT ALL ON TABLE "public"."courses" TO "anon";
+GRANT ALL ON TABLE "public"."courses" TO "authenticated";
+GRANT ALL ON TABLE "public"."courses" TO "service_role";
+
+GRANT ALL ON TABLE "public"."lesson_blocks" TO "anon";
+GRANT ALL ON TABLE "public"."lesson_blocks" TO "authenticated";
+GRANT ALL ON TABLE "public"."lesson_blocks" TO "service_role";
+
+GRANT ALL ON TABLE "public"."lessons" TO "anon";
+GRANT ALL ON TABLE "public"."lessons" TO "authenticated";
+GRANT ALL ON TABLE "public"."lessons" TO "service_role";
+
+GRANT ALL ON TABLE "public"."profiles" TO "anon";
+GRANT ALL ON TABLE "public"."profiles" TO "authenticated";
+GRANT ALL ON TABLE "public"."profiles" TO "service_role";
+
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON SEQUENCES  TO "postgres";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON SEQUENCES  TO "anon";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON SEQUENCES  TO "authenticated";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON SEQUENCES  TO "service_role";
+
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON FUNCTIONS  TO "postgres";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON FUNCTIONS  TO "anon";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON FUNCTIONS  TO "authenticated";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON FUNCTIONS  TO "service_role";
+
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON TABLES  TO "postgres";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON TABLES  TO "anon";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON TABLES  TO "authenticated";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON TABLES  TO "service_role";
+
+RESET ALL;
+
+--
+-- Dumped schema changes for auth and storage
+--
+
+CREATE OR REPLACE TRIGGER "on_auth_user_created" AFTER INSERT ON "auth"."users" FOR EACH ROW EXECUTE FUNCTION "public"."handle_new_user"();
+
+CREATE POLICY "Anyone can upload an avatar." ON "storage"."objects" FOR INSERT WITH CHECK (("bucket_id" = 'avatars'::"text"));
+
+CREATE POLICY "Avatar images are publicly accessible." ON "storage"."objects" FOR SELECT USING (("bucket_id" = 'avatars'::"text"));
+
