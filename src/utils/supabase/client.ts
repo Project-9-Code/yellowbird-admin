93657eca3efac1c86589575808118859dc0f7f3b
CB <chris@project9.studio>
2024-05-10 10:06:56 -0400

    Changed env

--- src/utils/supabase/client.ts
+++ src/utils/supabase/client.ts
@@ -1,10 +1,10 @@
 import { Database } from "@/database.types";
 import { createBrowserClient } from "@supabase/ssr";
-console.log(process.env.NEXT_PUBLIC_TEST)
+console.log(process.env.NEXT_PUBLIC_SUPABASE_URL)
 
 export function createClient() {
   return createBrowserClient<Database>(
-    process.env.NEXT_PUBLIC_SB_URL!,
-    process.env.NEXT_PUBLIC_SB_ANON_KEY!
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   )
 }
